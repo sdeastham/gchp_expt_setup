42d6b754ad22213c98db9ec2686d841bfe3acf00
Sebastian D. Eastham <seastham@mit.edu>
2019-12-08 13:55:01 -0500

    The get_n_days script can now return both the number of days which have passed AND the number of days in the total simution (pass argument "both"). Running the command ./setup_multi_seg.sh $( ../get_n_days.py both ) will therefore schedule all (and ONLY) the simulations needed to complete the period specific in runConfig.sh

--- get_n_days.py
+++ get_n_days.py
@@ -10,6 +10,7 @@ else:
    output_arg = 'short'
 
 full_output = output_arg == 'long'
+both_output = output_arg == 'both'
 
 curr_date = None
 with open('cap_restart') as fp:
@@ -20,21 +21,30 @@ with open('cap_restart') as fp:
 
 #base_date = calendar.datetime.datetime(2014,7,1,0,0,0)
 base_date_str = None
+stop_date_str = None
 with open('runConfig.sh','r') as f:
     for line in f.readlines():
         if 'Start_Time=' in line:
             base_date_str=line.split('"')[1]
+        elif 'End_Time=' in line:
+            stop_date_str=line.split('"')[1]
 assert base_date_str is not None, 'Start date not found!'
 base_date = calendar.datetime.datetime.strptime(base_date_str,'%Y%m%d %H%M%S')
+stop_date = calendar.datetime.datetime.strptime(stop_date_str,'%Y%m%d %H%M%S')
 
 #print(base_date)
 curr_date = calendar.datetime.datetime.strptime(curr_date,'%Y%m%d %H%M%S')
 #print(curr_date)
 n_days_passed = (curr_date - base_date).days
+n_days_total  = (stop_date - base_date).days
 
 if full_output:
    print('In directory {:s}'.format(os.path.basename(os.getcwd())))
    print(' --> Simulation has reached {:s}'.format(curr_date.strftime('%Y-%m-%d')))
    print(' --> Number of days passed since {:s}: {:d}'.format(base_date.strftime('%Y-%m-%d'),n_days_passed))
+   print(' --> Number of days to end date  {:s}: {:d}'.format(stop_date.strftime('%Y-%m-%d'),n_days_total))
+elif both_output:
+   print(n_days_passed)
+   print(n_days_total)
 else:
    print(n_days_passed)
